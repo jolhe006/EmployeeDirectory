bd5920af1ec3f4b45aade734134cd1b36bd542d4
jolhe006 <jlpr51@gmail.com>
2020-05-07 21:56:04 -0500

    updated bootstrap v3.4.1 to v4.4.1

--- CODE/EmployeeDirectory/EmployeeDirectory/App_Start/BundleConfig.cs
+++ CODE/EmployeeDirectory/EmployeeDirectory/App_Start/BundleConfig.cs
@@ -20,7 +20,11 @@ namespace EmployeeDirectory
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/popper.js",
                       "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/appjs").Include(
+                      "~/Scripts/app.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
